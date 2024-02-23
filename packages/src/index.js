f3c2ef9f3b99fb3692258ac905194cb5781cb618
Timothy Headley <theadley@jhb.dvt.co.za>
2024-02-23 12:18:40 +0200

    feat: :sparkles: ESLint, Prettier, and date-fns

--- packages/src/index.js
+++ packages/src/index.js
@@ -1,4 +1,10 @@
 import { format } from "date-fns";
 
 const date = new Date();
-console.log(format(date, "yyyy-MM-dd"));
+console.log(format(date, `yyyy-MM-dd`));
+
+const arr = [1, 2, 2, 3, 5, 4];
+
+if (true) {
+  console.log(`Yes`);
+}
